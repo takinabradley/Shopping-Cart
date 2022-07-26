7b54d5e780ea6fc74a9b4ef5585fc73fa86198a0
takinabradley <bradfinney98@gmail.com>
2022-07-26 01:03:37 -0400

    move RouteSwitch components into src/components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import RouteSwitch from './RouteSwitch';
+import RouteSwitch from './components/RouteSwitch';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
