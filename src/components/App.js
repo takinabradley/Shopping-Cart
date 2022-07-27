644e13c5ee92cd558531eb5e752372b20f79485f
takinabradley <bradfinney98@gmail.com>
2022-07-27 19:31:11 -0400

    pass products list through outlet context

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "./Navbar";
+import products from "../products"
 
 function App() {
 
   return (
     <div className="App">
       <Navbar />
-      <Outlet />
+      
+      <Outlet context={{products}}/>
     </div>
   );
 }
