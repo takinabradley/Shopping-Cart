314b749d189b20291c1ee7514257b907b6de6ab4
takinabradley <bradfinney98@gmail.com>
2022-07-29 01:49:38 -0400

    add description and image to products

--- src/products.js
+++ src/products.js
@@ -1,9 +1,30 @@
 import uniquid from "uniquid";
 
 const products = {
-  product1: { name: 'product1', price: 150, stock: 3, id: uniquid()},
-  product2: { name: 'product2', price: 30, stock: 15, id: uniquid()},
-  product3: { name: 'product3', price: 25, stock: 12, id: uniquid()}
+  product1: {
+    name: 'product1',
+    img: '',
+    description: 'a product',
+    price: 150,
+    stock: 3,
+    id: uniquid()
+  },
+  product2: {
+    name: 'product2',
+    img: '',
+    description: 'a product',
+    price: 30,
+    stock: 15,
+    id: uniquid()
+  },
+  product3: {
+    name: 'product3',
+    img: '',
+    description: 'a product',
+    price: 25,
+    stock: 12,
+    id: uniquid()
+  }
 }
 
 export default products;
