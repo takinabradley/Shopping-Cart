324d9b6be46b49c694b15ee5849421218edac6f8
takinabradley <bradfinney98@gmail.com>
2022-07-31 04:02:55 -0400

    add quantity input to productCard component

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 export default function ProductCard({product}) {
-  
+  const [quantity, setQuantity] = useState(0)
+
+  function onChangeQuantity(e) {
+    setQuantity(e.target.value)
+  }
+
   return (
     <div className='product-card' data-testid='productCard'>
       <div className="product-name">{product.name}</div>
 
       <img className="product-img" src={product.img} alt={product.name} />
 
+      <input className='product-quantity' type="number" value={quantity} onChange={onChangeQuantity} />
       <div className="product-description">{product.description}</div>
       <div className="product-stock">{product.stock}</div>
     </div>
--- src/components/ProductCard.test.js
+++ src/components/ProductCard.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import "@testing-library/jest-dom"
 import { screen, render } from '@testing-library/react'
 import ProductCard from "./ProductCard";
+import userEvent from "@testing-library/user-event"
 
 it('renders a card', () => {
   const product = {}
@@ -22,9 +23,21 @@ it('renders a card correctly', () => {
     productCard: screen.getByTestId('productCard'),
     productName: screen.getByText('product'),
     productDescription: screen.getByText('a product'),
+    productQuantity: screen.getByDisplayValue(0),
     productImg: screen.getByAltText('product'),
     productStock: screen.getByText('3')
   }
   
   Object.values(elems).forEach(elem => expect(elem).toBeInTheDocument())
+})
+
+it('updates quantity when changed', () => {
+  const product = {
+    name: 'product', description: 'a product', img: 'img', stock: 3, id: '0000'
+  }
+  render(<ProductCard product={product} />)
+
+  const productQuantity = screen.getByDisplayValue(0)
+  userEvent.type(productQuantity, '{backspace}1')
+  expect(productQuantity.value).toBe('1')
 })
