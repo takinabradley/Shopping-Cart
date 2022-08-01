b85f8fb0bdd7c9a2c7179c289cf6689af5035125
takinabradley <bradfinney98@gmail.com>
2022-07-31 21:43:01 -0400

    create cart state Shop component

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,10 +1,11 @@
 import ProductCard from "./ProductCard"
 
-export default function ProductList({products}) {
-  const productCards = Object.values(products).map( product =>
-    <ProductCard product={product} key={product.id} />
-  )
+export default function ProductList({ products, addToCart }) {
+  
 
+  const productCards = Object.values(products).map( product =>
+    <ProductCard product={product} addToCart={addToCart} key={product.id} />
+  )
   return (
     <div data-testid='productList' className="product-list">
       {productCards}
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,15 +1,34 @@
-import React from 'react'
-import { useOutletContext } from 'react-router-dom'
-import ProductList from './ProductList'
-import "./Shop.css"
+import React, { useState } from "react";
+import { useOutletContext } from "react-router-dom";
+import ProductList from "./ProductList";
+import "./Shop.css";
 
 export default function Shop() {
   // || ensures we're destructuring an object if function returns undefined
-  const { products, setProducts } = useOutletContext() || {}
-  
+  const { products, setProducts } = useOutletContext() || {};
+  const [cart, setCart] = useState({});
+
+  function checkOut() {}
+
+  function removeFromCart(product) {
+    const newCart = { ...cart };
+    delete newCart[product.name];
+    setCart(newCart);
+  }
+
+  function addToCart(product) {
+    const newCart = { ...cart, [product.name]: product };
+    setCart(newCart);
+    console.log("cart:", newCart);
+  }
+
+  const productList = products ? (
+    <ProductList products={products} addToCart={addToCart} />
+  ) : null;
+
   return (
-    <div data-testid='shop'>
-      {products ? <ProductList products={products}/> : null}
+    <div data-testid="shop">
+      {productList}
     </div>
-  )
+  );
 }
