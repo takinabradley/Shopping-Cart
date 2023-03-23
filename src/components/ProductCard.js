f22715be072a59de089687d5ed8cdddb43040cb8
takinabradley <bradfinney98@gmail.com>
2023-03-23 11:07:00 -0400

    move product title below image

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -6,10 +6,10 @@ const BEM = createBEM("product-card")
 export default function ProductCard({ product, handleAddToCart, modifiers }) {
   return (
     <div key={product.id} className={`${BEM.b} ${BEM.bm(modifiers)}`}>
-      <h2 className={BEM.e("title")}>{product.title}</h2>
       <div className={BEM.e("img-container")}>
         <img src={img2} alt="" className={BEM.e("img")} />
       </div>
+      <h2 className={BEM.e("title")}>{product.title}</h2>
       <p className={BEM.e("description")}>{product.description}</p>
       <div className={BEM.e("price")}>{product.price}</div>
       <button
