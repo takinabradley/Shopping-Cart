3b513caf5d80bad92fe5e15593f3cd46bdf73efb
takinabradley <bradfinney98@gmail.com>
2023-03-24 02:47:27 -0400

    BEMify Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,14 +6,14 @@ const BEM = createBEM("hero")
 function Hero({ image, heading, blurb, buttonText, handleClick, modifiers }) {
   return (
     <div className={`hero ${BEM.bm(modifiers)}`}>
-      <div className="hero__img-container">
-        <img src={image} alt="featured product" className="hero__img" />
+      <div className={BEM.e("img-container")}>
+        <img src={image} alt="featured product" className={BEM.e("img")} />
       </div>
 
-      <div className="hero__info">
-        <h2 className="hero__heading">{heading}</h2>
-        <p className="hero__blurb">{blurb}</p>
-        <button className="hero__learn-more" onClick={handleClick}>
+      <div className={BEM.e("info")}>
+        <h2 className={BEM.e("heading")}>{heading}</h2>
+        <p className={BEM.e("blurb")}>{blurb}</p>
+        <button className={BEM.e("learn-more")} onClick={handleClick}>
           {buttonText}
         </button>
       </div>
