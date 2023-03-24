bf539bbf6d87797bb4c3377463634b30e3703772
takinabradley <bradfinney98@gmail.com>
2023-03-24 04:39:41 -0400

    add FixMe note

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -8,9 +8,12 @@ export default function Banner({ children, modifiers }) {
 
 function ShopBanner({ children, modifiers }) {
   return (
+    // FixMe related to BEMNames:
+    // adding default extended modifiers is a bit weird. Maybe an array would be
+    // better
     <Banner
       modifiers={`shop-banner transparent-black-background${
-        modifiers ? " " + modifiers : ""
+        modifiers ? modifiers.padStart(1) : ""
       }`}
     >
       {children}
