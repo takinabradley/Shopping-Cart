333754af1b215105a54a9a6325d5be73140f908d
takinabradley <bradfinney98@gmail.com>
2023-03-27 17:38:47 -0400

    add homepage content and styles

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,12 +1,30 @@
 import createBEM from "@takinabradley/bem-names"
+import "../component-styles/HomePage.scss"
 import React from "react"
 import Block from "./Block"
-
+import Hero from "./Hero.js"
+import img1 from "../images/1.jpg"
 const BEM = createBEM("home")
 export default function HomePage({ modifiers }) {
   return (
     <Block BEM={BEM} modifiers={modifiers}>
-      <h1 className={BEM.e("heading")}>I'm the home page!</h1>
+      <Hero
+        image={img1}
+        heading="Hero Banner"
+        blurb={`\
+This. Is. A. Hero. banner.
+It's here to show off a cool product.
+`}
+        buttonText="Click. Here."
+        modifiers="transparent-black-background"
+      />
+      <Hero
+        image={img1}
+        heading="Hero Banner"
+        blurb="This. Is. Another. One. It's just here to fill some space"
+        buttonText="Click. Here."
+        modifiers="transparent-black-background"
+      />
     </Block>
   )
 }
